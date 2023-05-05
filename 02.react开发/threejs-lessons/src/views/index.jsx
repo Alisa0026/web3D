c6d1df3be7558697c7ed21dd5053934797731f72
yql <yuanql@foxmail.com>
2023-05-05 10:26:16 +0800

    feat: 解决锯齿问题，设置渲染器屏幕像素比，提高渲染精度

--- 02.react开发/threejs-lessons/src/views/index.jsx
+++ 02.react开发/threejs-lessons/src/views/index.jsx
@@ -14,8 +14,20 @@ const Page = () => {
 
 
     // three.js
+
+    /**
+     * 1. 有锯齿
+     * 2. 没有3D立体效果
+     * 3. 6 个面能不能有不同颜色
+     */
+
     // 创建3D场景对象
     const sence = new THREE.Scene()
+    // 创建辅助坐标系
+    const axesHelper = new THREE.AxesHelper()
+
+    sence.add(axesHelper)
+
     // 创建立方体的几何体
     const geometry = new THREE.BoxGeometry(1, 1, 1)
     // 创建立方体的基础材质
@@ -28,17 +40,17 @@ const Page = () => {
 
     // 创建相机对象，透视相机:可以模拟人眼观察角度，近大远小
     const aspect = width / height;
-    // const camera = new THREE.PerspectiveCamera(45, aspect, 1)
+    const camera = new THREE.PerspectiveCamera(45, aspect, 1)
 
     // TODO:正交相机：参数是：左右上下前后
-    const camera = new THREE.OrthographicCamera(
-      -aspect,
-      aspect,
-      aspect,
-      -aspect,
-      0.01,
-      100
-    )
+    // const camera = new THREE.OrthographicCamera(
+    //   -aspect,
+    //   aspect,
+    //   aspect,
+    //   -aspect,
+    //   0.01,
+    //   100
+    // )
 
     // 设置相机位置
     camera.position.set(2, 2, 3) // x:2 y:2 z:3
@@ -49,8 +61,14 @@ const Page = () => {
 
     // 创建渲染器
     const renderer = new THREE.WebGLRenderer({
-      canvas: canvas
+      canvas: canvas,
+      // 1.解决锯齿：抗锯齿效果
+      antialias: true,
     })
+
+    // 设置渲染器屏幕像素比，提高渲染精度
+    renderer.setPixelRatio(window.devicePixelRatio || 1)
+
     // 设置渲染器大小
     renderer.setSize(width, height)
     // 执行渲染
